3ad5b4bb3ae1a113cab10f3e0c04710e2b64c77b
Akhil Shukla <akhil.shukla@flipkart.com>
2021-05-06 14:29:44 +0530

    minor change

--- src/components/pages/thankyoupage/Thankyou.jsx
+++ src/components/pages/thankyoupage/Thankyou.jsx
@@ -3,22 +3,27 @@ import { connect } from "react-redux";
 import './styles.scss';
 import thankyou from "../../../assets/thankyou.png";
 import thankgirl from "../../../assets/thankgirl.png";
-import { useHistory} from "react-router-dom";
-import {clearCart} from '../../../redux/cart/cart.actions'
+// import { useHistory} from "react-router-dom";
+import {clearCart} from '../../../redux/cart/cart.actions';
+import { Link } from "react-router-dom";
 
 function Thankyou(props) {
-    const redirect = useHistory();
+    // const redirect = useHistory();
     const {clearCartAsync,cartItems}= props;
 
     useEffect(()=>{
         // console.log("below is cart")
         // console.log(JSON.parse(localStorage.getItem("cartItems")));
-        if(cartItems){
-            clearCartAsync()
+        if(cartItems.length > 0){
+            clearCartAsync();
         }
       
         
     })
+
+    // const goToHome = () => {
+    //     redirect.push("/");
+    // }
 
 
     return (
@@ -26,14 +31,16 @@ function Thankyou(props) {
             <div className="thanks">
                 <h1>this must be visible</h1>
                 {/* <img src={thankyou} alt="something"></img> */}
-                <div style={{background:`url(${thankyou})`, height:"600px",objectFit:"cover" , width:"auto",display:"flex",flexDirection:"column"}}>
+                <div style={{background:`url(${thankyou})`, height:"600px",objectFit:"cover" , width:"auto",display:"flex",flexDirection:"column",alignItems:"center"}}>
 
                    
                    <div className="tgirl">
                     <img src={thankgirl} alt="thankyougirl"></img>
                    </div>
-                   <div>
-                   <button id="btn" onClick={()=>redirect.push('/')}>Continue Shopping</button>
+                   <div className="redlink">
+                   {/* <button id="btn" onClick={goToHome}>Continue Shopping</button> */}
+
+                   <Link to="/">Continue shopping</Link>
                    </div>
                     
                     
