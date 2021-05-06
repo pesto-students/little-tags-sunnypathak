b2bfbd6139c558ec924f9477c5c522c9658d6544
Akhil Shukla <akhil.shukla@flipkart.com>
2021-05-06 18:54:12 +0530

    minor change

--- src/components/pages/thankyoupage/Thankyou.jsx
+++ src/components/pages/thankyoupage/Thankyou.jsx
@@ -1,19 +1,18 @@
 import React,{useEffect} from 'react';
 import { connect } from "react-redux";
 import './styles.scss';
-import thankyou from "../../../assets/thankyou.png";
+
 import thankgirl from "../../../assets/thankgirl.png";
-// import { useHistory} from "react-router-dom";
+
 import {clearCart} from '../../../redux/cart/cart.actions';
 import { Link } from "react-router-dom";
 
 function Thankyou(props) {
-    // const redirect = useHistory();
+    
     const {clearCartAsync,cartItems}= props;
 
     useEffect(()=>{
-        // console.log("below is cart")
-        // console.log(JSON.parse(localStorage.getItem("cartItems")));
+      
         if(cartItems.length > 0){
             clearCartAsync();
         }
@@ -21,26 +20,22 @@ function Thankyou(props) {
         
     })
 
-    // const goToHome = () => {
-    //     redirect.push("/");
-    // }
-
 
     return (
-        <div>
+        <div className="thq-container">
             <div className="thanks">
-                <h1>this must be visible</h1>
+                
                 {/* <img src={thankyou} alt="something"></img> */}
-                <div style={{background:`url(${thankyou})`, height:"600px",objectFit:"cover" , width:"auto",display:"flex",flexDirection:"column",alignItems:"center"}}>
+                <div className="thqbackground">
 
                    
                    <div className="tgirl">
                     <img src={thankgirl} alt="thankyougirl"></img>
                    </div>
                    <div className="redlink">
-                   {/* <button id="btn" onClick={goToHome}>Continue Shopping</button> */}
+                   
 
-                   <Link to="/">Continue shopping</Link>
+                    <Link to="/">Continue shopping</Link>
                    </div>
                     
                     
