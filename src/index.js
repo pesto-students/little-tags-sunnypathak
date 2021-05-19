315439ab065914e9d3f29456682a25f6e291deb9
unknown <sp140898@gmail.com>
2021-05-19 23:24:43 +0530

    remove extra line'

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 // Store Import
 import store from "./redux/store";
 import App from "./App";
- //import AddDataToFirebase from "./firebase/addDataHelper/AddDataToFirebase";
+//import AddDataToFirebase from "./firebase/addDataHelper/AddDataToFirebase";
 // CSS Import
 import "./index.css";
 
@@ -14,7 +14,6 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
-      {/* Run if new data is required to add inside Firestore with new data in shop.data.js */}
       {/* <AddDataToFirebase /> */}
     </BrowserRouter>
   </Provider>,
